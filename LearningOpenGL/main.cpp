2293760881d9eb1ba53e14d5597fc5e60239f711
张毅 <zyallday@qiyi.com>
2022-11-11 14:22:22 +0800

    Initial Commit

---
+++ LearningOpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearningOpenGL
+//
+//  Created by lostjobs on 2022/11/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
